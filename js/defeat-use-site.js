54fe0773c2a1fdd7440586e7e575c0b8eecff0b2
prohor33 <prohorgladkikh@gmail.com>
2016-01-06 19:22:18 +0300

    add google analytics

---
+++ js/defeat-use-site.js
@@ -0,0 +1,11 @@
+
+
+function go_to_appl_store() {
+  ga('send', 'event', 'Reference', 'click', 'Apple Store');
+  location.href='https://itunes.apple.com/us/app/pobedi-ege-2016-matematika/id997503005';
+}
+
+function go_to_google_play() {
+  ga('send', 'event', 'Reference', 'click', 'Google Play');
+  location.href='https://play.google.com/store/apps/details?id=crystal.tech.defeat_use';
+}
